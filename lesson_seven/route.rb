a16a6a90cc4746e8917566450a1ee1a9e576ecf0
assa555@mai.ru <80923241023assa>
2022-04-29 16:36:31 +0300

    start

---
+++ lesson_seven/route.rb
@@ -0,0 +1,44 @@
+class Route
+  include InstanceCounter
+  include Validatable
+
+  attr_reader :start_station, :last_station, :middle_stations
+
+  def initialize(start_station, last_station)
+    @start_station = start_station
+    @last_station = last_station
+    @middle_stations = []
+    register_instance
+    validate!
+  end
+
+  def add_station(station)
+    if stations.include?(station)
+      puts 'Такая станция уже присутсвует в маршруте'
+    else
+      @middle_stations << station
+    end
+  end
+
+  def del_station(station)
+    if stations.include?(station)
+      @middle_stations.delete(station)
+    else
+      puts 'Такой станции нет в маршруте'
+    end
+  end
+
+  def stations
+    [@start_station, @middle_stations, @last_station].flatten
+  end
+
+  def show
+    puts (stations.each { |station| puts station.name }).to_s
+  end
+
+  def validate!
+    return true if stations.map(&:is_a?).all?
+
+    raise ValidationError, 'Недопустимый ввод. Укажите пожалуйста станцию'
+  end
+end
